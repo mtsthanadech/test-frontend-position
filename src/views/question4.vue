9b7d321073bfdc21ccb2dd6e6ad4fbf26a8febaf
mtsthanadech <mts.thanadech@gmail.com>
2020-02-03 02:36:55 +0700

    add parent component of the profile in question4

---
+++ src/views/question4.vue
@@ -0,0 +1,33 @@
+<template>
+  <profile
+    :name="user.name"
+    :job="user.job"
+    :posts="user.posts"
+    :likes="user.likes"
+    :followers="user.followers"/>
+</template>
+
+<script>
+  import profile from "../components/profile";
+  export default {
+    name: "question4",
+    components: {
+      profile
+    },
+    data() {
+      return {
+        user: {
+          name: "Jessica Potter",
+          job: "Visual Artist",
+          posts: 523,
+          likes: 1387,
+          followers: 146
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
