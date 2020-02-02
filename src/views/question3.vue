6a0717b616e4357feca5a60402cebc5f94e29c75
mtsthanadech <mts.thanadech@gmail.com>
2020-02-03 01:58:28 +0700

    add find unique of the question 3

---
+++ src/views/question3.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    findUniq({{set0}}) return {{findUniq(set0)}}
+    <br>
+    findUniq({{set1}}) return {{findUniq(set1)}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "question3",
+    data() {
+      return {
+        set0: [1, 1, 1, 2, 1, 1],
+        set1: [0, 0, 0.55, 0, 0]
+      }
+    },
+    methods: {
+      findUniq(values) {
+        let arr = [];
+        let dup = [];
+
+        for (let i = 0; i < values.length; i++) {
+          if (!arr.includes(values[i])) {
+            // Add values are unique to arr
+            arr.push(values[i]);
+          } else {
+            // Add values are duplicate to dup
+            dup.push(values[i]);
+          }
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+          if (dup.includes(arr[i])) {
+            // Remove duplicate value in arr by compare with dup
+            arr.splice(i,1);
+          }
+        }
+
+        // Return the first value of the arr
+        return arr[0]
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
