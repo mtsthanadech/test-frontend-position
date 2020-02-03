2844f43d79a94bde31a38cb6067c059f7c04c7fb
mtsthanadech <mts.thanadech@gmail.com>
2020-02-03 23:33:40 +0700

    import font-awesome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@/assets/font-awesome-4.7.0/css/font-awesome.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
